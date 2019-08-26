29144e7f73a9f23f12e59aa36c930ca4a6aed0d1
lyukio <lyukio@hotmail.com>
2019-08-25 21:50:19 -0300

    creating rootQuery with author and books and mutation with addAuthor and addBook

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express()
 
+mongoose.connect('mongodb://lyukio:lyukio123@ds351987.mlab.com:51987/graphql-ninja', { useNewUrlParser: true })
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+})
+
 app.use('/graphql', graphqlHTTP({
-    
+    schema,
+    graphiql: true
 }))
 
 app.listen(4000, () => {
